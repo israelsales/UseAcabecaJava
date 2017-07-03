0d97169ee8ffee35160abdc636c9fafe790ba0e8
Israel Sales <raell.com@gmail.com>
2017-07-03 12:39:23 -0300

    ...

---
+++ src/com/useacabecajava/beersong/BeerSong.java
@@ -0,0 +1,28 @@
+package com.useacabecajava.beersong;
+
+/**
+ * Created by Israel Sales on 03/07/2017.
+ */
+public class BeerSong {
+    public static void main(String[] args) {
+        int beerBum = 9;
+        String word = "bottles";
+
+        while (beerBum > 0) {
+            System.out.println(beerBum + " " + word + " of beer.");
+            System.out.println("Take one down.");
+            System.out.println("Pass it around.");
+            beerBum = beerBum - 1;
+
+            if (beerBum > 0) {
+                if (beerBum == 1) {
+                    word = "bottle";
+                }
+                System.out.println(beerBum + " " + word + " of beer on the wall");
+            } else {
+                System.out.println("No more bottles of beer on the wall");
+            }
+        }
+
+    }
+}
